550fd542ff8d4071d200b28c78aba46dddd83b24
beatrizsales00 <202602199726@alunos.estacio.br>
2026-02-15 22:04:22 +0000

    criei o printf

---
+++ super_trunfo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int carta1 = 1;
+    char estado[] = "Ceará";
+    char codigo_da_carta[] = "A01";
+    char nome_da_cidade[] = "Fortaleza";
+    int populacao = 2686612;
+    float area_km2 = 312.353;
+    float pib = 86.93;
+    int numero_de_pontos_turisticos = 20;
+
+    printf("Carta: %d\n", carta1);
+    printf("Estado: %s\n", estado);
+    printf("Código da carta: %s\n", codigo_da_carta);
+    printf("Nome da cidade: %s\n", nome_da_cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.3f km²\n", area_km2);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Número de pontos turísticos: %d\n", numero_de_pontos_turisticos);
+
+    int carta2 = 2;
+char estado_2[] = "Bahia";
+char codigo_da_carta_2[] = "A02";
+char nome_da_cidade_2[] = "Salvador";
+int populacao_2 = 2568928;
+float area_km2_2 = 693.83;
+float pib_2 = 62.954;
+int numero_de_pontos_turisticos_2 = 15;
+
+printf("Carta: %d\n", carta2);
+printf("Estado: %s\n", estado_2);
+printf("Código da carta: %s\n", codigo_da_carta_2);
+printf("Nome da cidade: %s\n", nome_da_cidade_2);
+printf("População: %d\n", populacao_2);
+printf("Área: %.2f km²\n", area_km2_2);
+printf("PIB: %.3f bilhões de reais\n", pib_2);
+printf("Número de pontos turísticos: %d\n", numero_de_pontos_turisticos_2);
+
+    return 0;
+}
